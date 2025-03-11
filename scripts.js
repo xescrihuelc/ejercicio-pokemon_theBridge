46192d9966b6a6474045340441998e4579841b1e
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-03-11 10:46:47 +0100

    Ejercicio 2 resuleto

--- scripts.js
+++ scripts.js
@@ -3,3 +3,7 @@ console.log(document.title);
 // Ejercicio 1
 const titleGen1 = document.getElementById("gen-1");
 titleGen1.innerText = "Generasión 1 Pokimon";
+
+// Ejercicio 2
+const pokeGen1 = document.querySelector(".infocard-list.infocard-list-pkmn-lg");
+pokeGen1.style.backgroundColor = "yellow";
